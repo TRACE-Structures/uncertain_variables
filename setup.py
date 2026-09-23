3d5c38fa3df267969f25b051a912477418b4b593
Popovics Bence <popbence@gmail.com>
2026-09-23 16:09:19 +0200

    Rename uncertain_variables folder to src and bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='uncertain_variables',
-    version='1.0.1',
+    version='1.0.2',
     author='András Urbanics, Bence Popovics, Emese Vastag, Elmar Zander, Noémi Friedman',
     author_email='popbence@hun-ren.sztaki.hu',
     description='Defining and handling variable sets with probability distributions for surrogate modelling and uncertainty quantification applications',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/TRACE-Structures/uncertain_variables/',
-    packages=find_packages(),
-    py_modules=['uncertain_variables'],
+    packages=['uncertain_variables'] + ['uncertain_variables.' + p for p in find_packages(where='src')],
+    package_dir={'uncertain_variables': 'src'},
     install_requires=[
         'numpy',
         'scikit-learn',
